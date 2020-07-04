d6659e0a941393626522a09043b89a0233348f8b
NataLiaKech <nkechedzhieva@gmail.com>
2020-07-03 21:33:27 -0400

    practice git2

---
+++ src/main/java/GITprojectPractice2/GITprojectPractice2/App.java
@@ -0,0 +1,13 @@
+package GITprojectPractice2.GITprojectPractice2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
